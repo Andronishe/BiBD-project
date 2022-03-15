2dc622e657d6df28256039753a4bc5541e4307d9
Andrey Matskevich <wedas018@mail.ru>
2022-03-15 14:09:16 +0300

    second commit add t_users

--- database_code.sql
+++ database_code.sql
@@ -5,7 +5,7 @@ create table t_game_category
     id       int auto_increment
         primary key,
     category varchar(255) not null,
-    constraint t_game_categories_name_uindex
+    constraint t_book_categories_name_uindex
         unique (category),
     constraint t_game_categories_id_uindex
         unique (id)
@@ -17,7 +17,7 @@ create table t_games_authors
         primary key,
     company varchar(100) not null,
     email   varchar(100) not null,
-    constraint t_game_authors_email_uindex
+    constraint t_book_authors_email_uindex
         unique (email),
     constraint tb_id_unique
         unique (id)
@@ -34,7 +34,7 @@ create table t_games
     category     int default 1 not null,
     constraint tb_id_unique
         unique (id),
-    constraint t_games_author_id_fk
+    constraint t_books_author_id_fk
         foreign key (author_id) references t_games_authors (id)
             on update cascade on delete cascade,
     constraint t_games_category_id_fk
@@ -43,10 +43,10 @@ create table t_games
 )
     charset = utf8;
 
-create index t_gamess_publish_date_index
+create index t_books_publish_date_index
     on t_games (publish_date);
 
-create index t_games_authors_firstname_lastname_index
+create index t_book_authors_firstname_lastname_index
     on t_games_authors (company);
 
 create table t_gamestores
@@ -68,18 +68,28 @@ create table t_gamestore_games
     game_id  int                        not null,
     count    int                        not null,
     price    decimal(5, 2) default 0.00 not null,
-    constraint t_gamestore_games_store_id_game_id_uindex
+    constraint t_bookstore_books_store_id_book_id_uindex
         unique (store_id, game_id),
     constraint tbs_id_unique
         unique (id),
-    constraint t_gamestore_games_game_id_fk
+    constraint t_bookstore_books_book_id_fk
         foreign key (game_id) references t_games (id)
             on update cascade on delete cascade,
-    constraint t_gamestore_games_store_id_fk
+    constraint t_bookstore_books_store_id_fk
         foreign key (store_id) references t_gamestores (id)
             on update cascade on delete cascade
 )
     charset = utf8;
+
+create table t_users
+(
+    idu      int auto_increment
+        primary key,
+    login    varchar(20) not null,
+    password varchar(20) not null,
+    constraint t_users_login_uindex
+        unique (login)
+);
 
 create table t_gamestore_sold_games
 (
@@ -91,29 +101,33 @@ create table t_gamestore_sold_games
     status      bit           default b'0' not null,
     insert_date date                       not null,
     update_date date                       not null,
+    user_id     int                        null,
     constraint tbs_id_unique
         unique (id),
-    constraint t_gamestore_sold_store_id_games_id_fk
+    constraint t_bookstore_sold_store_id_books_id_fk
         foreign key (game_id) references t_games (id),
     constraint t_bookstore_sold_store_id_fk
         foreign key (store_id) references t_gamestores (id)
+            on delete cascade,
+    constraint t_gamestore_sold_games_user_id_fk
+        foreign key (user_id) references t_users (idu)
             on delete cascade
 )
     charset = utf8;
 
-create index t_gamestore_sold_games_status_index
+create index t_bookstore_sold_books_status_index
     on t_gamestore_sold_games (status);
 
-create index t_gamestore_sold_games_update_date_index
+create index t_bookstore_sold_books_update_date_index
     on t_gamestore_sold_games (update_date);
 
-create definer = root@localhost trigger t_gamestore_sold_games_before_insert
+create definer = root@localhost trigger t_bookstore_sold_books_before_insert
     before insert
     on t_gamestore_sold_games
     for each row
     SET NEW.insert_date = NOW(), NEW.update_date = NOW();
 
-create definer = root@localhost trigger t_gamestore_sold_games_before_update
+create definer = root@localhost trigger t_bookstore_sold_books_before_update
     before update
     on t_gamestore_sold_games
     for each row
